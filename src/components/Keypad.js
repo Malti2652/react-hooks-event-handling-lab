8ac037f1ed58f4cca40e5ab905809f85eda17748
Eddington Rowellis <eddingtonibi@gmail.com>
2023-02-12 19:31:25 +0300

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,36 @@
 // Code Keypad Component Here
 
-function Keypad (){
+// function Keypad (){
+//     return (
+//         <div></div>
+//     )
+// }
+
+// export default Keypad;
+
+import React from 'react';
+
+function Keypad() {
+class Keypad extends React.Component {
+  render() {
     return (
-        <div></div>
-    )
+      <div>
+        <input type="password" />
+      </div>
+    );
+  }
+}
+
+
+  const handleChange = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
